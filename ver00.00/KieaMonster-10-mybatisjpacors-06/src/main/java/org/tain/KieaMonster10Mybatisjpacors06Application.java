667ed4aae40b146f533963785674122faee4be49
grtlinux <grtlinux@gmail.com>
2021-06-12 18:54:05 +0900

    create 1st

---
+++ ver00.00/KieaMonster-10-mybatisjpacors-06/src/main/java/org/tain/KieaMonster10Mybatisjpacors06Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaMonster10Mybatisjpacors06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaMonster10Mybatisjpacors06Application.class, args);
+	}
+
+}
