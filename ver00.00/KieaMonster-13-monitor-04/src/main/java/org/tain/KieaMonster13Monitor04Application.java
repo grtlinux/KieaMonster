f09bd11879e83e3dc5a36ab94d026f8b8b841459
grtlinux <grtlinux@gmail.com>
2021-06-13 14:45:52 +0900

    create 1st

---
+++ ver00.00/KieaMonster-13-monitor-04/src/main/java/org/tain/KieaMonster13Monitor04Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaMonster13Monitor04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaMonster13Monitor04Application.class, args);
+	}
+
+}
