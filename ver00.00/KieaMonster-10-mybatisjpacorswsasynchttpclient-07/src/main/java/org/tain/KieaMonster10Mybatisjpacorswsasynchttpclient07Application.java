001d4ed701b1a1cff1ddd8f9d0b19a9e41258c6d
grtlinux <grtlinux@gmail.com>
2021-06-15 16:27:40 +0900

    create 1st

---
+++ ver00.00/KieaMonster-10-mybatisjpacorswsasynchttpclient-07/src/main/java/org/tain/KieaMonster10Mybatisjpacorswsasynchttpclient07Application.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaMonster10Mybatisjpacorswsasynchttpclient07Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaMonster10Mybatisjpacorswsasynchttpclient07Application.class, args);
+	}
+
+}
